4c83c534f3406403042956387a40533f6ca0e296
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-05-12 15:43:31 +0800

    add debouncer to handle rapid file changes

---
+++ internal/notify/debounce.go
@@ -0,0 +1,23 @@
+package notify
+
+import "time"
+
+type Debouncer struct {
+	timer *time.Timer
+	used  bool
+}
+
+// Run runs a given func after the given delay. If Run is called within the delay period, the delay will reset.
+// Ensures the given func is run only once after the last call.
+func (d *Debouncer) Run(delay time.Duration, f func()) {
+	if d.used {
+		d.timer.Stop()
+	} else {
+		d.used = true
+	}
+
+	d.timer = time.AfterFunc(delay, func() {
+		d.used = false
+		f()
+	})
+}
