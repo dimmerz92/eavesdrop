adddec6ff155adbe78ff6df68d452fc0031da55a
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-05-13 23:02:19 +0800

    add script injector func and tests

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,12 +1,18 @@
 package proxy
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/dimmerz92/eavesdrop/internal/config"
 )
+
+const SSE_SCRIPT = "<script>hello</script>"
 
 type Proxy struct {
 	AppPort   int
@@ -37,26 +43,74 @@ func NewProxy(cfg *config.Config) *Proxy {
 // proxyRequest forwards the request to the target app server, injects a server sent event script for automatic
 // browser refreshing.
 func (p *Proxy) proxyRequest(w http.ResponseWriter, r *http.Request) {
-	req, err := http.NewRequest(r.Method, fmt.Sprintf("http://localhost:%d", p.AppPort), r.Body)
+	// create a forwarding request
+	req, err := http.NewRequest(r.Method, fmt.Sprintf("http://localhost:%d%s", p.AppPort, r.URL.Path), r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	req.Header = r.Header.Clone()
-	resp, err := p.Client.Do(req)
+	req.Header.Set("X-Forwarded-For", r.RemoteAddr)
+	req.Header.Set("Via", fmt.Sprintf("%s %s", r.Proto, r.Host))
+
+	// forward the request
+	var resp *http.Response
+	for i := 0; i < 10; i++ {
+		resp, err = p.Client.Do(req)
+		if err == nil {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadGateway)
+		http.Error(w, "proxy error: application unresponsive", http.StatusBadGateway)
 		return
 	}
 	defer resp.Body.Close()
 
 	for k, v := range resp.Header {
 		for _, vv := range v {
+			if k == "Content-Length" {
+				continue
+			}
 			w.Header().Add(k, vv)
 		}
 	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Via", fmt.Sprintf("%s %s", r.Proto, r.Host))
 
 	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
+
+	if !strings.Contains(resp.Header.Get("Content-Type"), "text/html") {
+		w.Header().Set("Content-Length", resp.Header.Get("Content-Length"))
+		io.Copy(w, resp.Body)
+	} else {
+		body, err := p.injectRefresher(resp)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("proxy error: %v", err), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Length", strconv.Itoa(len([]byte(body))))
+		if _, err := io.WriteString(w, body); err != nil {
+			http.Error(w, fmt.Sprintf("proxy error: %v", err), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+// injectRefresher injects the sse script if the body exists.
+func (p *Proxy) injectRefresher(resp *http.Response) (string, error) {
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(resp.Body); err != nil {
+		return "", fmt.Errorf("proxy error: failed to read body")
+	}
+	page := buf.String()
+
+	// get the index of the body closing tag
+	body := strings.LastIndex(page, "</body>")
+	if body == -1 {
+		return page, nil
+	}
+
+	return page[:body] + SSE_SCRIPT + page[body:], nil
 }
--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -1,6 +1,7 @@
 package proxy_test
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"testing"
@@ -10,8 +11,13 @@ import (
 	"github.com/dimmerz92/eavesdrop/internal/proxy"
 )
 
-const (
-	html = "<!DOCTYPE html><html><head><title>test</title><body></body></html>"
+var (
+	shouldInject    = "<!DOCTYPE html><html><head><title>test</title><body></body></html>"
+	shouldNotInject = "<!DOCTYPE html><html><head><title>test</title></html>"
+	injected        = fmt.Sprintf(
+		"<!DOCTYPE html><html><head><title>test</title><body>%s</body></html>",
+		proxy.SSE_SCRIPT,
+	)
 )
 
 func makeServer() *http.Server {
@@ -19,12 +25,22 @@ func makeServer() *http.Server {
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
-		w.Write([]byte(html))
+		w.Write([]byte("get"))
 	})
 
 	mux.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("posted"))
+	})
+
+	mux.HandleFunc("GET /inject-me", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(shouldInject))
+	})
+
+	mux.HandleFunc("GET /dont-inject-me", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(shouldNotInject))
 	})
 
 	return &http.Server{
@@ -33,7 +49,7 @@ func makeServer() *http.Server {
 	}
 }
 
-func TestProxy_ProxyRequest(t *testing.T) {
+func TestProxy(t *testing.T) {
 	origin := makeServer()
 	originErrChan := make(chan error, 1)
 
@@ -69,55 +85,93 @@ func TestProxy_ProxyRequest(t *testing.T) {
 	default:
 	}
 
-	t.Run("test successful GET through proxy", func(t *testing.T) {
-		resp, err := http.Get("http://localhost:8001")
-		if err != nil {
-			t.Fatalf("proxy request failed: %v", err)
-		}
-		defer resp.Body.Close()
+	t.Run("test proxyRequest", func(t *testing.T) {
+		t.Run("test successful GET through proxy", func(t *testing.T) {
+			resp, err := http.Get("http://localhost:8001")
+			if err != nil {
+				t.Fatalf("proxy request failed: %v", err)
+			}
+			defer resp.Body.Close()
 
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatalf("failed to read response body: %v", err)
-		}
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("failed to read response body: %v", err)
+			}
 
-		if string(body) != html {
-			t.Errorf("expected: %s\ngot: %s", html, string(body))
-		}
+			if string(body) != "get" {
+				t.Errorf("expected: get\ngot: %s", string(body))
+			}
+		})
+
+		t.Run("test successful POST through proxy", func(t *testing.T) {
+			resp, err := http.Post("http://localhost:8001", "text/plain", nil)
+			if err != nil {
+				t.Fatalf("proxy request failed: %v", err)
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("failed to read response body: %v", err)
+			}
+
+			if string(body) != "posted" {
+				t.Errorf("expected: %s\ngot: %s", "posted", string(body))
+			}
+		})
+
+		t.Run("test PUT not allowed through proxy", func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodPut, "http://localhost:8001", nil)
+			if err != nil {
+				t.Fatalf("creating request failed: %v", err)
+			}
+
+			client := http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				t.Fatalf("proxy request failed: %v", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusMethodNotAllowed {
+				t.Errorf("expected status: %d got: %d", http.StatusMethodNotAllowed, resp.StatusCode)
+			}
+		})
 	})
 
-	t.Run("test successful POST through proxy", func(t *testing.T) {
-		resp, err := http.Post("http://localhost:8001", "text/plain", nil)
-		if err != nil {
-			t.Fatalf("proxy request failed: %v", err)
-		}
-		defer resp.Body.Close()
+	t.Run("test injectRefresher", func(t *testing.T) {
+		t.Run("test successful injection", func(t *testing.T) {
+			resp, err := http.Get("http://localhost:8001/inject-me")
+			if err != nil {
+				t.Fatalf("proxy request failed: %v", err)
+			}
+			defer resp.Body.Close()
 
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatalf("failed to read response body: %v", err)
-		}
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("failed to read response body: %v", err)
+			}
 
-		if string(body) != "posted" {
-			t.Errorf("expected: %s\ngot: %s", "posted", string(body))
-		}
-	})
+			if string(body) != injected {
+				t.Errorf("expected: %s\ngot: %s", injected, string(body))
+			}
+		})
 
-	t.Run("test PUT not allowed through proxy", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodPut, "http://localhost:8001", nil)
-		if err != nil {
-			t.Fatalf("creating request failed: %v", err)
-		}
+		t.Run("test successful non-injection", func(t *testing.T) {
+			resp, err := http.Get("http://localhost:8001/dont-inject-me")
+			if err != nil {
+				t.Fatalf("proxy request failed: %v", err)
+			}
+			defer resp.Body.Close()
 
-		client := http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			t.Fatalf("proxy request failed: %v", err)
-		}
-		defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("failed to read response body: %v", err)
+			}
 
-		if resp.StatusCode != http.StatusMethodNotAllowed {
-			t.Errorf("expected status: %d got: %d", http.StatusMethodNotAllowed, resp.StatusCode)
-		}
+			if string(body) != shouldNotInject {
+				t.Errorf("expected: %s\ngot: %s", shouldNotInject, string(body))
+			}
+		})
 	})
 }
