4325e53e5b56bdb4d9604ed6e8b383444661afc3
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-05-14 13:27:08 +0800

    cleanup debounce

--- internal/notify/debounce.go
+++ internal/notify/debounce.go
@@ -7,7 +7,8 @@ type Debouncer struct {
 	used  bool
 }
 
-// Run runs a given func after the given delay. If Run is called within the delay period, the delay will reset.
+// Run runs a given func after the given delay. If Run is called within the
+// delay period, the delay will reset.
 // Ensures the given func is run only once after the last call.
 func (d *Debouncer) Run(delay time.Duration, f func()) {
 	if d.used {
