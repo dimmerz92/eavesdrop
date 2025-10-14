480968726232b1d2306c9a124368ec71c78af22b
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-10-14 23:01:35 +0800

    add debouncer

---
+++ debouncer.go
@@ -0,0 +1,24 @@
+package eavesdrop
+
+import "time"
+
+type Debouncer struct {
+	timer *time.Timer
+	used  bool
+}
+
+// Run executes the function after the delay has passed.
+// Repeat calls to Run will reset the timer.
+// Run does not check if the given function is always the same.
+func (d *Debouncer) Run(delay time.Duration, f func()) {
+	if d.used {
+		d.timer.Stop()
+	} else {
+		d.used = true
+	}
+
+	d.timer = time.AfterFunc(delay, func() {
+		d.used = false
+		f()
+	})
+}
